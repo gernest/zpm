e709614ae5e4d506cbf003f7248887fba2c6b162
gernest <geofreyernest@live.com>
2018-12-13 20:00:52 +0300

    initial work

--- build.zig
+++ build.zig
@@ -3,13 +3,15 @@ const Builder = @import("std").build.Builder;
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
     const exe = b.addExecutable("zpm", "src/main.zig");
+    exe.addPackagePath("clap", "lib/clap/src/index.zig");
+    exe.addPackagePath("semver", "lib/semver/src/main.zig");
     exe.setBuildMode(mode);
+    var main_tests = b.addTest("src/main.zig");
+    main_tests.addPackagePath("semver", "lib/semver/src/main.zig");
+    main_tests.setBuildMode(mode);
 
-    const run_step = b.step("run", "Run the app");
-    const run_cmd = b.addCommand(".", b.env_map, [][]const u8{exe.getOutputPath()});
-    run_step.dependOn(&run_cmd.step);
-    run_cmd.step.dependOn(&exe.step);
-
+    const test_step = b.step("test", "Run app tests");
+    test_step.dependOn(&main_tests.step);
     b.default_step.dependOn(&exe.step);
     b.installArtifact(exe);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,29 @@
 const std = @import("std");
+const warn = std.debug.warn;
+const semver = @import("semver");
 
-pub fn main() anyerror!void {
-    std.debug.warn("All your base are belong to us.\n");
+const Package = struct {
+    name: ?[]const u8,
+    version: ?[]const u8,
+    versions: ?[]const []const u8,
+    main: bool,
+    directory: ?[]const u8,
+    package_file: ?[]const u8,
+    zig_version: ?[]const u8,
+};
+
+const Version = struct {
+    name: []const u8,
+    version: []const u8,
+
+    pub fn check(self: *const Version) bool {
+        return semver.isValid(self.version);
+    }
+};
+
+test "Version" {
+    const v = Version{ .name = "zpm", .version = "v0.1.0" };
+    warn("check {}\n", v.check());
 }
+
+pub fn main() void {}
