11a5575d46706e10daa8e032d48ce4f1952c9bfb
gernest <geofreyernest@live.com>
2018-12-14 09:00:46 +0300

    cleanup build.zig

--- build.zig
+++ build.zig
@@ -1,13 +1,31 @@
-const Builder = @import("std").build.Builder;
+const build_pkg = @import("std").build;
+const Builder = build_pkg.Builder;
+
+const Pkg = struct {
+    name: []const u8,
+    path: []const u8,
+};
+
+const packages = struct {
+    const list = []Pkg{
+        Pkg{ .name = "clap", .path = "lib/clap/src/index.zig" },
+        Pkg{ .name = "semver", .path = "lib/semver/src/main.zig" },
+        Pkg{ .name = "zson", .path = "lib/zson/src/main.zig" },
+    };
+    fn addTo(ts: var) void {
+        for (list) |p| {
+            ts.addPackagePath(p.name, p.path);
+        }
+    }
+};
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
     const exe = b.addExecutable("zpm", "src/main.zig");
-    exe.addPackagePath("clap", "lib/clap/src/index.zig");
-    exe.addPackagePath("semver", "lib/semver/src/main.zig");
+    packages.addTo(exe);
     exe.setBuildMode(mode);
     var main_tests = b.addTest("src/main.zig");
-    main_tests.addPackagePath("semver", "lib/semver/src/main.zig");
+    packages.addTo(main_tests);
     main_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run app tests");
